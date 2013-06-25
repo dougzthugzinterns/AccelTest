8bcb77b5e0981ff20dc96bf53b8369efece0bdf0
dougzthugzinterns <christc5@egb-10-33-10-188.muo.muohio.edu>
2013-06-25 14:45:53 -0400

    6th

--- AccellorometerReadTest/AccellorometerReadTestViewController.cs
+++ AccellorometerReadTest/AccellorometerReadTestViewController.cs
@@ -43,18 +43,26 @@ namespace AccellorometerReadTest
 			currentMaxRotZ = 0;
 
 			_motionManager = new CMMotionManager ();
-			_motionManager.StartAccelerometerUpdates (NSOperationQueue.CurrentQueue, (data,error) =>
+			_motionManager.StartDeviceMotionUpdates (NSOperationQueue.CurrentQueue, (data,error) =>
 			{
-				this.accX.Text = data.Acceleration.X.ToString("0.0000");
-				this.accY.Text = data.Acceleration.Y.ToString("0.0000");
-				this.accZ.Text = data.Acceleration.Z.ToString("0.0000");
+				this.accX.Text = data.UserAcceleration.X.ToString("0.0000");
+				this.accY.Text = data.UserAcceleration.Y.ToString("0.0000");
+				this.accZ.Text = data.UserAcceleration.Z.ToString("0.0000");
 
-				this.maxAccX.Text = Math.Sqrt ((data.Acceleration.X * data.Acceleration.X) + 
-				                               (data.Acceleration.Y * data.Acceleration.Y) +
-				                               (data.Acceleration.Z * data.Acceleration.Z)).ToString("0.0000");
+				this.rotX.Text = data.RotationRate.x.ToString("0.0000");
+				this.rotY.Text = data.RotationRate.y.ToString("0.0000");
+				this.rotZ.Text = data.RotationRate.z.ToString("0.0000");
+
+				this.maxRotX.Text = data.Attitude.ToString("0.0000");
+
+				this.maxAccX.Text = Math.Sqrt ((data.UserAcceleration.X * data.UserAcceleration.X) + 
+				                               (data.UserAcceleration.Y * data.UserAcceleration.Y) +
+				                               (data.UserAcceleration.Z * data.UserAcceleration.Z)).ToString("0.0000");
 			
 
 			});
+
+
 
 
 			// Perform any additional setup after loading the view, typically from a nib.
